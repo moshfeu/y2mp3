00abe427e3d37f0d846cf2825987701aabc08a4a
Mosh Feu <moshfeu@gmail.com>
2018-10-20 01:23:12 +0300

    add typescript

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 const YoutubeMp3Downloader = require('youtube-mp3-downloader');
 const ytlist = require('youtube-playlist');
 
-function fetchVideos(playlistUrl, callback) {
+function fetchVideos(playlistUrl: string, callback: (playlist: string[]) => void) {
   //Configure YoutubeMp3Downloader with your settings
   var YD = new YoutubeMp3Downloader({
       "ffmpegPath": "/Users/moshef/bin/JDownloader\ 2.0/tools/mac/ffmpeg_10.6+/ffmpeg",        // Where is the FFmpeg binary located?
@@ -11,11 +11,11 @@ function fetchVideos(playlistUrl, callback) {
       "progressTimeout": 1000                 // How long should be the interval of the progress reports
   });
 
-  ytlist(playlistUrl, 'id').then(data => {
+  ytlist(playlistUrl, 'id').then((data: any) => {
     console.log('fetch videos from platlist', data);
     const { data: {playlist} } = data;
     callback(playlist);
-    playlist.forEach(videoId => {
+    playlist.forEach((videoId: string) => {
       YD.download(videoId);
     });
   });
@@ -23,21 +23,21 @@ function fetchVideos(playlistUrl, callback) {
   //Download video and save as MP3 file
   // YD.download("JvKKd32Yw2E");
 
-  YD.on("finished", function(err, data) {
+  YD.on("finished", function() {
       // console.log(JSON.stringify(data, null, 2));
       console.log('done');
   });
 
-  YD.on("error", function(error) {
+  YD.on("error", function(error: string) {
       console.log(error);
   });
 
 
-  YD.on("progress", function(progress) {
+  YD.on("progress", function(progress: any) {
     console.log(JSON.stringify(progress, null, 2));
   });
 
-  YD.on("queueSize", function(size) {
+  YD.on("queueSize", function(size: string) {
     console.log('size', size);
   });
 }
