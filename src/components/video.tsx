7c3d80679903984bd46de0b5d3bfc2cf372bd248
shayanypn <shayanypn@gmail.com>
2019-03-26 23:07:46 +0430

    make song title a link

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import { IVideoEntity, EVideoStatus } from '../types';
 import { ButtonProgress } from './button-progress';
+import { shell } from '../services/electron-adapter';
 
 interface IVideoProps {
   style?: React.CSSProperties,
@@ -19,6 +20,11 @@ export class Video extends React.Component<IVideoProps, any> {
     return `url(https://img.youtube.com/vi/${this.props.video.id}/mqdefault.jpg)`;
   }
 
+  onClickTitle():void {
+    const { video:{id} } = this.props;
+    shell.openExternal(`https://www.youtube.com/watch?v=${this.props.video.id}`);
+  }
+
   render() {
     const { video, onVideoDownloadClick, style } = this.props;
     const { backgroundImage } = this;
@@ -28,7 +34,8 @@ export class Video extends React.Component<IVideoProps, any> {
     return (
       <div className="video" style={{backgroundImage, ...style}}>
         <div className="details">
-          <div className="name">
+          <div className="name"
+            onClick={this.onClickTitle.bind(this)}>
             {video.name}
           </div>
           <div className="button">
