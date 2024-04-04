9e705ce54765bce0e754c4d108a5d03f0c12c90e
Moshe Feuchtwanger <moshfeu@gmail.com>
2024-04-04 23:09:06 +0200

    fix: error with check for updates

--- src/services/check-for-update.ts
+++ src/services/check-for-update.ts
@@ -1,14 +1,36 @@
-import fetch from 'electron-fetch';
 import { ipcRenderer } from 'electron';
+import axios from 'axios';
 import { showAppHasUpdate } from './modalsAndAlerts';
 import store from '../mobx/store';
 
+const fetch = <T extends unknown>(url: string) => new Promise<{
+  json: () => T,
+  text: () => string,
+}>(async (resolve, reject) => {
+  try {
+    const request = await axios.request({
+      url,
+      method: 'GET',
+    });
+    if (request.status >= 200 && request.status < 300) {
+      resolve({
+        json: () => request.data,
+        text: () => JSON.stringify(request.data),
+      });
+    } else {
+      reject(request.statusText);
+    }
+  } catch (error) {
+    console.error(error);
+    reject(error);
+  }
+});
 
 export async function checkForUpdateAndNotify() {
   const currentVersion = await ipcRenderer.invoke('version');
-  const latestVersion = await fetch('https://api.github.com/repos/moshfeu/y2mp3/releases/latest')
-                                .then(data => data.json())
-                                .then(data => data.name.replace('v', ''));
+  const latestVersion = await fetch<{ name: string }>('https://api.github.com/repos/moshfeu/y2mp3/releases/latest')
+    .then(data => data.json())
+    .then(data => data.name.replace('v', ''));
   if (currentVersion !== latestVersion) {
     store.hasUpdate = true;
     showAppHasUpdate();
