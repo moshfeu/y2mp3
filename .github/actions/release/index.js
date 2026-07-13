e6c8050717c32b32abc7b255b656e23093a06a80
Mosh Feu <moshfeu@gmail.com>
2026-07-13 23:16:02 +0300

    fix github action syntax

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -1,6 +1,6 @@
-const { execSync } = require("child_process");
-const { existsSync, readFileSync } = require("fs");
-const { join } = require("path");
+import { execSync } from "node:child_process";
+import { existsSync, readFileSync } from "node:fs";
+import { join } from "node:path";
 
 /**
  * Logs to the console
